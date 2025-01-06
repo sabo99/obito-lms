adf163a6a17fe48e1cf2e27e936e9c54b026e1ba
Alvin Setiawan <alvin.set25@gmail.com>
2025-01-07 00:56:12 +0700

    feat(filament): #DEV-06 add laravel permissions

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -4,14 +4,15 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\RoleResource\Pages;
 use App\Filament\Resources\RoleResource\RelationManagers;
-use App\Models\Role;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Permission\Models\Role;
 
 class RoleResource extends Resource
 {
@@ -23,7 +24,10 @@ class RoleResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->label('Role Name')
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 
@@ -31,10 +35,13 @@ class RoleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Role Name')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
-                Tables\Filters\TrashedFilter::make(),
+                // Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
