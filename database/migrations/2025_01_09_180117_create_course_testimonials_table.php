c67fd4d5cf091bafff86d9a7ce0b38fec2bc9d70
Alvin Setiawan <alvin.set25@gmail.com>
2025-01-10 01:36:34 +0700

    feat(relation-manager): #DEV-08 add courseResource RelationManager

---
+++ database/migrations/2025_01_09_180117_create_course_testimonials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('courses')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('comment');
+            $table->double('rating');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_testimonials');
+    }
+};
